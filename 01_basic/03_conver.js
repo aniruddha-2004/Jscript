9d1e77226227114073a7af069efffea6eb2386dd
aniruddha-2004 <karmakaraniruddha497@gmail.com>
2024-09-06 05:50:38 +0000

    conversion of datatype

---
+++ 01_basic/03_conver.js
@@ -0,0 +1,28 @@
+let score = 33
+
+console.log(typeof score);
+
+score = "33";
+console.log(typeof (score));
+
+score = "33abc"
+
+let valin_number = Number(score)
+console.log(typeof valin_number)
+console.log(valin_number);
+
+// "33" => 33
+//"33abdc" => Nan 
+//true => 1   false => 0
+
+let isloggedIn = "toton"
+let boolis = Boolean(isloggedIn)
+console.log(boolis);
+
+let somenumber = 55
+let stringnum = String(somenumber)
+console.log(stringnum);
+
+console.log(typeof stringnum);
+
+
